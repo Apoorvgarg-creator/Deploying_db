fc9a980759c3f4403e559e385932e4d0b0841517
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-05-31 17:01:28 +0530

    path error resolved

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4444
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.get('/', (req,res) => {
-    res.render('<h1> Deploying website + database</h1>')
+    res.send('<h1> Deploying website + database</h1>')
 })
 app.get('/tasks',async (req,res) => {
     res.send(await Tasks.findAll())
